3aeadcda26d3db2f56353daee4156eb31b211452
natashaannn <natashaannlum@gmail.com>
2019-04-22 15:06:33 -0700

    Added notredame model and adjusted camera

--- js/map.js
+++ js/map.js
@@ -1,45 +1,59 @@
-//Creating the scene
+//Load 3D Scene
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+//Load Camera Perspective
+var camera = new THREE.PerspectiveCamera(
+  100, //Field of view
+  window.innerWidth / window.innerHeight, //Aspect Ratio
+  1, //Near
+  20000 //Far
+);
+camera.position.set( -10000, 1000, -4800 );
+
+//Load a Renderer
+var renderer = new THREE.WebGLRenderer({ alpha: false });
+renderer.setClearColor( 0x000000 );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//Notredame
+// Load the Orbitcontroller
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+// Load Light
+var ambientLight = new THREE.AmbientLight( 0xcccccc );
+scene.add( ambientLight );
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff );
+directionalLight.position.set( 0, 1, 1 ).normalize();
+scene.add( directionalLight );
+
+//glTf Loader
 var loader = new THREE.GLTFLoader();
+  loader.load(
+    'js/notredame/scene.gltf',
+    function ( gltf ) {
+      var object = gltf.scene;
+      gltf.scene.scale.set( 2, 2, 2 );
+	    gltf.scene.position.x = 0;				    //Position (x = right+ left-)
+      gltf.scene.position.y = 0;				    //Position (y = up+, down-)
+	    gltf.scene.position.z = 0;
+  	  scene.add(gltf.scene);
+     },
+     // undefined,
+     // function ( error ) {
+  	 //    console.log('an error occurred');
+     //  }
+    );
 
-loader.load(
-  'js/scene.gltf',
-  function ( gltf ) {
-	   scene.add( gltf.scene );
-   },
-   function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	 },
-   function ( error ) {
-	    console.error( error );
-    }
-  );
-
-// //Cube
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x008080 } );
-// var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-// camera.position.z = 5;
-
-//Loop for Rendering the Cube
-// function animate() {
-// 	requestAnimationFrame( animate );
-//   cube.rotation.x += 0.01; //rotating the cube
-//   cube.rotation.y += 0.01;
-// 	renderer.render( scene, camera );
-// }
-// animate();
-
-//Loop for Rendering Notre Dame
-function render () {
+//Loop for Rendering
+function animate() {
+  render();
+  requestAnimationFrame(animate);
+	}
+function render() {
   renderer.render(scene,camera);
-  requestAnimationFrame(render);
 }
+
+render();
+animate();
