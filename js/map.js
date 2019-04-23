f259e860972fec6ab845bc8a3acb84b510f750cc
natashaannn <natashaannlum@gmail.com>
2019-04-23 09:31:23 -0700

    Updated map.js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,8 @@
+//WebGL Error Message
+if ( WEBGL.isWebGLAvailable() === false ) {
+  document.body.appendChild( WEBGL.getWebGLErrorMessage() );
+}
+
 //Load 3D Scene
 var scene = new THREE.Scene();
 
@@ -15,18 +20,31 @@ var renderer = new THREE.WebGLRenderer({ alpha: false });
 renderer.setClearColor( 0x000000 );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild( renderer.domElement );
 
 // Load the Orbitcontroller
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = 0.9 * Math.PI / 2;
 
-// Load Light
+// Load Surrounding Light
 var ambientLight = new THREE.AmbientLight( 0xcccccc );
 scene.add( ambientLight );
 
 var directionalLight = new THREE.DirectionalLight( 0xffffff );
 directionalLight.position.set( 0, 1, 1 ).normalize();
 scene.add( directionalLight );
+
+//Ground
+var groundgeometry = new THREE.PlaneBufferGeometry( 18000000, 9000000 );
+var groundmat = new THREE.MeshPhongMaterial ( { color: 0xd3d3d3} );
+var ground = new THREE.Mesh( groundgeometry, groundmat );
+ground.position.y = - 500;
+ground.receiveshadow = true;
+ground.rotation.x = - Math.PI / 2.0;
+scene.add( ground );
+
 
 //Model glTf Loader
 var loader = new THREE.GLTFLoader();
@@ -35,29 +53,34 @@ var loader = new THREE.GLTFLoader();
     function ( gltf ) {
       var object = gltf.scene;
       gltf.scene.scale.set( 2, 2, 2 );
-	    gltf.scene.position.x = 0;				    //Position (x = right+ left-)
-      gltf.scene.position.y = 0;				    //Position (y = up+, down-)
+	    gltf.scene.position.x = 0;
+      gltf.scene.position.y = 0;
 	    gltf.scene.position.z = 0;
+      gltf.castShadow = true;
+      gltf.receiveShadow = false;
   	  scene.add(gltf.scene);
      },
-     // undefined,
-     // function ( error ) {
-  	 //    console.log('an error occurred');
-     //  }
+     undefined,
+     function ( error ) {
+  	    console.log('an error occurred');
+      }
     );
-
-//Load plane
-var planeGeometry = new THREE.PlaneGeometry( 5000, 100 );
-var material = new THREE.MeshBasicMaterial( {color: 0x000000, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
 
 //Light spheres
 var sphere = new THREE.SphereBufferGeometry( 200, 100, 8 );
-var light1 = new THREE.PointLight( 0xffa700, 2.5, 50000 );
+sphere.opacity = 0.5;
+var light1 = new THREE.PointLight( 0xffa700, 2.5, 25000 );
 light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffa700 } ) ) );
 light1.position.set( 3000, 0, -8000 );
+light1.castShadow = true;
+light1.opacity=0.5;
 scene.add( light1 );
+
+//Set up shadow properties for the light
+light1.shadow.mapSize.width = 18000;
+light1.shadow.mapSize.height = 9000;
+light1.shadow.camera.near = 1;
+light1.shadow.camera.far = 1000;
 
 //Loop for Rendering
 function animate() {
