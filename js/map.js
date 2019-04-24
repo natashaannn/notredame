72a165548b8a265730f9f51324ad6db04f0ca540
natashaannn <natashaannlum@gmail.com>
2019-04-23 23:17:17 -0700

    updates light spheres

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ var container = document.getElementById( 'container' );
 
 //Load 3D Scene
 var scene = new THREE.Scene();
-scene.fog = new THREE.Fog( scene.background, 3500, 15000 );
+var clock = new THREE.Clock();
 
 //Load Camera Perspective
 var camera = new THREE.PerspectiveCamera(
@@ -72,8 +72,8 @@ var loader = new THREE.GLTFLoader();
 
 //light sphere loop
 var emoji = ["a", "b", "c"];
-var lat = [5000, 6000, 7000];
-var long = [7000, 8000, 9000];
+var lat = [-5000, -6000, -7000];
+var long = [-7000, -8000, -9000];
 
 for (var i=0; i<4; i++) {
   var hue;
@@ -88,12 +88,29 @@ for (var i=0; i<4; i++) {
     hue = 0x6699ff;
   }
 
-  var sphere = new THREE.SphereBufferGeometry( 200, 100, 8 );
-  var light1 = new THREE.PointLight( hue, 2.5, 25000 );
-  light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: hue, transparent: true } ) ) );
+  var sphere = new THREE.SphereBufferGeometry( 10 );
+  var light1 = new THREE.PointLight( hue, 2.5, 250000, 2 );
+  light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: hue, transparent: true, opacity: 0.3 } ) ) );
   light1.position.set( lat[i], 0, long[i] );
-  light1.castShadow = true;
+  light1.castShadow = false;
   scene.add( light1 );
+
+  var spriteMap = new THREE.TextureLoader().load( "images/glow.png" );
+  var spriteMaterial = new THREE.SpriteMaterial(
+  {
+    map: spriteMap,
+    color: hue,
+    transparency: true,
+    opacity: 0.3,
+    blending: THREE.AdditiveBlending
+  });
+  var sprite = new THREE.Sprite( spriteMaterial );
+  sprite.scale.set(1000, 1000, 1000);
+  light1.add(sprite);
+  var sprite1 = new THREE.Sprite( spriteMaterial );
+  sprite1.scale.set(500, 500, 500);
+  light1.add(sprite1);
+
 }
 
 //Set up shadow properties for the light
@@ -102,13 +119,27 @@ light1.shadow.mapSize.height = 9000000;
 light1.shadow.camera.near = 1;
 light1.shadow.camera.far = 1000;
 
+// SUPER SIMPLE GLOW EFFECT
+// use sprite because it appears the same from all angles
+
+
 
 //Loop for Rendering
 function animate() {
   render();
   requestAnimationFrame(animate);
 	}
+
 function render() {
+
+  // var time = Date.now() * 0.000000000005;
+  //
+  // light1.scale.x = time;
+  // light1.scale.y = time;
+  // light1.scale.z = time;
+  //
+  // controls.update( clock.getDelta() );
+
   renderer.render(scene,camera);
 }
 
