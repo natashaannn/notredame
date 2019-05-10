c19f57659e564cdd4de85045274415abb244c356
natashaannn <natashaannlum@gmail.com>
2019-05-09 23:49:02 -0700

    Updated writeup and fix glitch for emoji cloud

--- js/emoji.js
+++ js/emoji.js
@@ -1,7 +1,7 @@
 EmojiCloud('#emoji-cloud-1', {
   data: [
     {
-      "unicode": "🚨",
+      "unicode": "1F6A8",
       "count": 115
     },
     {
@@ -77,91 +77,5 @@ EmojiCloud('#emoji-cloud-1', {
       "count": 4
     },
   ],
-  cssOptions: { height: '300', width: '900', }
+  cssOptions: { height: '300', width: '500', }
 });
-
-// EmojiCloud('#emoji-cloud-2', {
-//   data: [
-//     {
-//       "unicode": "😂",
-//       "count": 47
-//     },
-//     {
-//       "unicode": "🙏",
-//       "count": 28
-//     },
-//     {
-//       "unicode": "✅",
-//       "count": 22
-//     },
-//     {
-//       "unicode": "☘️",
-//       "count": 21
-//     },
-//     {
-//       "unicode": "👇",
-//       "count": 20
-//     },
-//     {
-//       "unicode": "⬇️",
-//       "count": 20
-//     },
-//     {
-//       "unicode": "🏈",
-//       "count": 17
-//     },
-//     {
-//       "unicode": "🤔",
-//       "count": 16
-//     },
-//     {
-//       "unicode": "🔥",
-//       "count": 16
-//     },
-//     {
-//       "unicode": "⤵",
-//       "count": 14
-//     },
-//     {
-//       "unicode": "😱",
-//       "count": 14
-//     },
-//     {
-//       "unicode": "➡️",
-//       "count": 13
-//     },
-//     {
-//       "unicode": "😢",
-//       "count": 13
-//     },
-//     {
-//       "unicode": "🔴",
-//       "count": 12
-//     },
-//     {
-//       "unicode": "💔",
-//       "count": 12
-//     },
-//     {
-//       "unicode": "😭",
-//       "count": 10
-//     },
-//     {
-//       "unicode": "❤️",
-//       "count": 9
-//     },
-//     {
-//       "unicode": "👇🏻🏽",
-//       "count": 9
-//     },
-//     {
-//       "unicode": "‼️",
-//       "count": 9
-//     },
-//     {
-//       "unicode": 🇫🇷",
-//       "count": 9
-//     },
-//   ],
-//   cssOptions: { height: '300', width: '900', }
-// });
---
+++ js/emoji2.js
@@ -0,0 +1,85 @@
+EmojiCloud('#emoji-cloud-2', {
+  data: [
+    {
+      "unicode": "😂",
+      "count": 47
+    },
+    {
+      "unicode": "🙏",
+      "count": 28
+    },
+    {
+      "unicode": "✅",
+      "count": 22
+    },
+    {
+      "unicode": "☘️",
+      "count": 21
+    },
+    {
+      "unicode": "👇",
+      "count": 20
+    },
+    {
+      "unicode": "⬇️",
+      "count": 20
+    },
+    {
+      "unicode": "🏈",
+      "count": 17
+    },
+    {
+      "unicode": "🤔",
+      "count": 16
+    },
+    {
+      "unicode": "🔥",
+      "count": 16
+    },
+    {
+      "unicode": "⤵",
+      "count": 14
+    },
+    {
+      "unicode": "😱",
+      "count": 14
+    },
+    {
+      "unicode": "➡️",
+      "count": 13
+    },
+    {
+      "unicode": "😢",
+      "count": 13
+    },
+    {
+      "unicode": "🔴",
+      "count": 12
+    },
+    {
+      "unicode": "💔",
+      "count": 12
+    },
+    {
+      "unicode": "😭",
+      "count": 10
+    },
+    {
+      "unicode": "❤️",
+      "count": 9
+    },
+    {
+      "unicode": "👇🏻🏽",
+      "count": 9
+    },
+    {
+      "unicode": "‼️",
+      "count": 9
+    },
+    {
+      "unicode": 🇫🇷",
+      "count": 9
+    },
+  ],
+  cssOptions: { height: '300', width: '500', }
+});
